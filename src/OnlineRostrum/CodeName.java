2ee3cd4607fe2b2428bf5d15ea6713bf0b1b8177
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-06-08 21:22:54 +0530

    Sum of chars in string

--- src/OnlineRostrum/CodeName.java
+++ src/OnlineRostrum/CodeName.java
@@ -3,12 +3,18 @@ package OnlineRostrum;
 public class CodeName {
 
 	public static void main(String[] args) {
-		String s = "super";
-		int sum=0;
-		s=s.toUpperCase();
-		for(int i=0;i<s.length();i++) {
-			sum+=s.charAt(i);
+		String s = "Super";
+		int sum = 0;
+		s = s.toUpperCase();
+		for (int i = 0; i < s.length(); i++) {
+			sum += s.charAt(i);
 		}
-		System.out.println(sum-s.length()*64);
+		System.out.println(sum - s.length() * 64);
+		
+		String s1="Super";  
+		System.out.println(s1.endsWith("r"));  
+		System.out.println(s1.endsWith("per"));  
+
 	}
+
 }
